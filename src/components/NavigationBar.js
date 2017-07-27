30162f931461d6654030e0f1d4daab1c37318d74
iloveyoukcl5770 <iloveyoukcl5770@gmail.com>
2017-07-27 01:19:55 -0500

    finished nav bar logic, which maintains a internal state that indicates which page the app is showing. add styling objects

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
-export default () => {
-    return (
-        <div>
-            This is nav-bar.
-        </div>
-    )
+import {styleNavLinkOnSelect} from '../../style/style'
+import {styleNavLinkUnSelect} from '../../style/style'
+
+export default class NavigationBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { selected: ""};
+        this.onNavBarClick = this.onNavBarClick.bind(this);
+    }
+    onNavBarClick(event){
+        if(event){
+            // update state only when a link being clicked
+            if(event.target.name){
+                this.setState({selected: event.target.name});
+            }
+        }
+    }
+    render(){
+        return (
+            <div onClick={this.onNavBarClick}>
+                <Link name="index" style={this.state.selected === "index" ? styleNavLinkOnSelect: styleNavLinkUnSelect} to="/" > index </Link>
+                <Link name="education" style={this.state.selected === "education" ? styleNavLinkOnSelect: styleNavLinkUnSelect} to="/education"> education </Link>
+                <Link name="work" style={this.state.selected === "work" ? styleNavLinkOnSelect: styleNavLinkUnSelect} to="/work"> work </Link>
+                <Link name="skills" style={this.state.selected === "skills" ? styleNavLinkOnSelect: styleNavLinkUnSelect} to="/skills"> skills </Link>
+                <Link name="projects" style={this.state.selected === "projects" ? styleNavLinkOnSelect: styleNavLinkUnSelect} to="/projects"> projects </Link>
+                <Link name="contact" style={this.state.selected === "contact" ? styleNavLinkOnSelect: styleNavLinkUnSelect} to="/contact"> contact </Link>
+            </div>
+        )
+    }
 }
---
+++ style/style.js
@@ -0,0 +1,9 @@
+
+
+export const styleNavLinkOnSelect = {
+    color: "black"
+};
+
+export const styleNavLinkUnSelect = {
+    color: "blue"
+};
