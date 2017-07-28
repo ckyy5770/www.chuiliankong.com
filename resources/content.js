c57e75945b8a2edf0d9a8ebcbc975403dacb2d23
iloveyoukcl5770 <iloveyoukcl5770@gmail.com>
2017-07-28 18:04:22 -0500

    update content

--- resources/content.js
+++ resources/content.js
@@ -9,10 +9,16 @@ export const educationContent = {
             startMonth: 8,
             endYear: 2018,
             endMonth: 5,
+            gpa: 3.7,
+            gpaStandard: 4.0,
             courses: [
-                "Operating System",
+                "Machine Learning",
+                "Concurrency Programming",
+                "Cloud Computing",
                 "Intermediate Software Design",
-                "Machine Learning"
+                "Advanced Algorithms",
+                "Distributed Systems",
+                "Operating System",
             ],
             honor:[
 
@@ -28,6 +34,8 @@ export const educationContent = {
             startMonth: 8,
             endYear: 2016,
             endMonth: 6,
+            gpa: 3.8,
+            gpaStandard: 4.0,
             courses: [
             ],
             honor:[
@@ -41,25 +49,103 @@ export const workContent = {
     workList: [
         {
             id:0,
-            company: "company1",
-            startYear: 2016,
-            startMonth: 8,
-            endYear: 2018,
-            endMonth: 5
+            company: "Vanderbilt University",
+            department: "Institute for Software Integrated Systems",
+            position: "Research and Development Engineer Intern",
+            startYear: 2017,
+            startMonth: 5,
+            endYear: 2017,
+            endMonth: 8,
+            keyWords: [
+                "Java",
+                "Python",
+                "ZeroMQ",
+                "ZooKeeper"
+            ],
+            descriptions: [
+                "Implemented an advanced Publisher-Subscriber Coordination System using ZeroMQ.",
+                "Designed and Implemented a discover service among clients and brokers using ZooKeeper.",
+                "Implemented an advanced load balancing logic based on channel replication, which reduced average transmission latency by 18%, comparing to classic consistent hashing method.",
+                "Wrote scripts for collecting system metrics, testing over Mininet and testing over cloud."
+            ]
         },
 
         {
             id:1,
-            company: "company2",
-            startYear: 2012,
-            startMonth: 8,
-            endYear: 2016,
-            endMonth: 6
+            company: "Huawei Technologies Co.Ltd.",
+            department: "Big Data Product Development",
+            position: "Software Engineer Intern",
+            startYear: 2015,
+            startMonth: 7,
+            endYear: 2015,
+            endMonth: 9,
+            keyWords: [
+                "C",
+                "Shell Script",
+                "Kernel Programming"
+            ],
+            descriptions: [
+                "Implemented a part of kernel module that is used for testing basic operations of a big-data storage system developed by Huawei. Brief description: to test whether basic operations generate correct quota log.",
+                "Designed and Implemented more than 50 test cases to test my part of functionality."
+            ]
         }
     ]
 };
 
 export const skillsContent = {
+    skillsList1: {
+        programmingLanguages:[
+            {
+                id:0,
+                skill: "Java",
+                proficiency: 80
+            },
+            {
+                id:1,
+                skill: "C++",
+                proficiency: 80
+            },
+            {
+                id:1,
+                skill: "C",
+                proficiency: 80
+            },
+            {
+                id:2,
+                skill: "Python",
+                proficiency: 60
+            },
+            {
+                id:2,
+                skill: "Javascript",
+                proficiency: 60
+            },
+        ],
+        libraries:[
+            {
+                id:0,
+                skill: "React",
+                proficiency: 60
+            },
+            {
+                id:1,
+                skill: "React-Native",
+                proficiency: 60
+            },
+            {
+                id:2,
+                skill: "Express",
+                proficiency: 60
+            },
+            {
+                id:3,
+                skill: "Bootstrap",
+                proficiency: 60
+            },
+        ]
+
+    },
+
     skillsList: [
         {
             id:0,
@@ -79,20 +165,43 @@ export const projectsContent = {
     projectsList: [
         {
             id:0,
-            projectName: "Project1",
-            startYear: 2016,
-            startMonth: 8,
-            endYear: 2018,
-            endMonth: 5
+            projectName: "Publisher-Subscriber System",
+            startYear: 2017,
+            startMonth: 4,
+            endYear: 2017,
+            endMonth: 5,
+            keyWords: [
+                "C++",
+                "ZeroMQ",
+                "Mininet"
+            ],
+            link: "https://github.com/iloveyoukcl5770/pub-sub-project-final",
+            descriptions: [
+                "Designed and Implemented an advanced Publisher-Subscriber System using ZeroMQ.",
+                "The system implements eight QoS policies inspired by Data Distributed Service, including but not limited to " +
+                "anonymous connection between publisher and subscriber, storing message history and enforcing a information " +
+                "strength to every Publisher."
+            ]
         },
 
         {
             id:1,
-            projectName: "Project2",
-            startYear: 2012,
-            startMonth: 8,
+            projectName: "Network Traffic Monitor",
+            startYear: 2016,
+            startMonth: 10,
             endYear: 2016,
-            endMonth: 6
+            endMonth: 12,
+            keyWords: [
+                "C",
+                "Shell Script",
+                "Kernel Programming"
+            ],
+            link: "https://github.com/iloveyoukcl5770/Network-Traffic-Monitor",
+            descriptions: [
+                "Designed and implemented a Linux kernel module using netfilter framework.",
+                "Implemented three major functionalities: monitoring network trac, blocking specific incoming/outgoing addresses and maintaining a quota for specified addresses.",
+                "Designed and implemented a set of test cases using shell script."
+            ]
         }
     ]
 };
